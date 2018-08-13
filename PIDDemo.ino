db5fe57f360d75ddb08803af6db79a63834a34b0
Philip Martel <pmartel@alum.mit.edu>
2018-08-13 19:24:30 -0400

    Angle mode and manual mode seem ok
    Adding a new table mode

--- PIDDemo.ino
+++ PIDDemo.ino
@@ -275,19 +275,33 @@ void waitFor( UL wait ) {
   lastTime = t;  
 }
 
+/********************************************************************************/
 // code for 'old' (ControlDemo) tests
 // states
 const char stIdle = 'i';
-const char stDisplay = 'a'; // angle display
+const char stAngle = 'a'; // angle display
 const char stManual = 'm';
 const char stBang = 'b';
+const char stExit = 'x';
 
 // globals
-char    state, oldState;
+char    state;
+bool    gotNewState;
+
+//function declarations to avoid errors, but why?
+/*
+void HelpIdle();
+void ProcessInput();
+void DisplayAngle();
+void ManualMotor();
+void HelpBang();
+void BangBang();
+*/
 
 void ControlDemoEntry() {
   myMotor->run(RELEASE); // make sure motor is stopped (coasting)
-  state = oldState = stIdle;
+  state =  stIdle;
+  gotNewState = true;
   HelpIdle();
   CDLoop();
 }
@@ -295,98 +309,212 @@ void ControlDemoEntry() {
 void CDLoop() {
   bool running = true;
   while ( running ) {
+    ProcessInput();
     switch( state ) {
     case stIdle :
-      HelpIdle();
+      if (gotNewState) {
+        HelpIdle();
+      }
+      gotNewState = false;
       break;
-    case stDisplay :
-      Serial << F(\
-      "Angle display mode\r\n"\
-      "q to return to Idle\r\n");
+    case stAngle :
+      if (gotNewState) {
+        Serial << F(\
+        "Angle display mode\r\n"\
+        "q to return to Idle\r\n");
+        gotNewState = false;
+      }
+      DisplayAngle();
       break;
     case stManual :
-      Serial << "Manual motor control mode \r\n";
-      Serial << "Enter a number -255 to 255 to set motor speed\r\n";
-      Serial << "q to stop motor return to Idle state\r\n";
+      if (gotNewState ) {
+        Serial << F(\
+        "Manual motor control mode \r\n"\
+        "Enter a number -255 to 255 to set motor speed\r\n"\
+        "q to stop motor return to Idle state\r\n");
+        gotNewState = false;
+      }
+      ManualMotor();
       break;
     case stBang :
-      HelpBang();
+      if ( gotNewState ){
+        HelpBang();
+        gotNewState = false;
+      }
+      BangBang();
       break;
     case stExit :
       running = false;
       myMotor->run(RELEASE); // make sure motor is stopped (coasting)
       break;
     default :
-      Serial << "Unknown state 0x" << _HEX(state) << endl;
-      Serial << "returning to Idle state\r\n";
+      Serial << F("Unknown state 0x") << _HEX(state) << endl;
+      Serial << F("returning to Idle state\r\n");
       Quit();
       break;
     }
   }  
+} // CDLoop()
+
+void BangBang() {
+  int inByte;
+  bool controllerOn = false;
+  int sp = 250;
+  float target;
+  float deadBand = 10;
+  
+  while ( stBang == state ) {
+    inByte = Serial.read();
+    switch( inByte ) {
+    case -1 : // no character
+      break;
+    case 'q' :
+      controllerOn = false;
+      Quit();
+      return;
+      break;
+    case 'h' :
+    case '?' :
+      HelpBang();
+      Serial << F("\r\nBang Bang parameters\r\n");
+      Serial << "Target angle = " << target << " deadband = " << deadBand << " motor speed = " << sp << endl;
+      break;
+    case 'a' : // set desired angle
+      target = Serial.parseFloat();
+      target = constrain( target, -20, 140);
+      Serial << "new target = " << target << endl;
+      break;
+    case 'd' : // set (half) deadband default 10
+      deadBand = Serial.parseFloat();
+      deadBand = constrain( deadBand, 0, 45);
+      Serial << "new deadband = " << deadBand << endl;
+      break;
+    case 'g' : // go - start controller
+      controllerOn = true;
+      break;
+    case 's' : // stop controller
+      controllerOn = false;
+      break;
+    case 'v' : // set motor speed (default = 150)
+      sp = Serial.parseInt();
+      sp = constrain( sp, 0, 255);
+      Serial << "new speed = " << sp << endl;
+      break;
+      break;
+    default: // bad input, just eat the character
+      break;
+    } 
+
+    if ( controllerOn ) {
+      float errorAng = ReadAngle() - target;
+
+      if ( Timer( 1000 ) ) {
+        Serial << "Target angle = " << target << " deadband = " << deadBand << " motor speed = " << sp << endl;
+        Serial <<  "Error = " << errorAng << endl << endl;
+      }
+      myMotor->setSpeed( sp );
+      if( abs( errorAng ) <= deadBand ) {
+        myMotor->run(RELEASE); // make sure motor is stopped (coasting);      
+      }
+      else if ( errorAng < 0 ) {
+        myMotor->run(FORWARD);
+      }
+      else {
+        myMotor->run(BACKWARD);    
+      }
+   }
+    else {
+      myMotor->run(RELEASE); // make sure motor is stopped (coasting);      
+    }
+  } // while in stBang 
+}
+
+void DisplayAngle() {
+  int inByte;
+  
+  angleRead = ReadAngle();  
+  // map it to the range of the analog out:
+  //outputValue = map(sensorValue, 0, 1023, 0, 255);
+
+  // print the results to the serial monitor:
+  Serial << "potCount  = " << potCount;
+  Serial << " angle = " << angleRead << "\r\n";
+  delay(1000); 
+  inByte = Serial.read();
+  if ( 'q' == inByte ) Quit();
+  if ( '?' == inByte || 'h' == inByte ) Serial << F("in Angle state 'q' to return to Idle state\r\n");
 }
 
 void HelpBang() {
-  Serial << "Bang bang controller mode\r\n";
-  Serial << "q - stop motor return to Idle state\r\n";
-  Serial << "a<number> - set target angle\r\n";
-  Serial << "d<number> - set (1/2) deadband\r\n";
-  Serial << "g - start\r\n";
-  Serial << "s - stop but stay in bang-bang mode\r\n";
-  Serial << "v<number> - set motor speed, 0-255 default 150\r\n\n";  
+  Serial << F(\
+  "Bang bang controller mode\r\n"\
+  "q - stop motor return to Idle state\r\n"\
+  "a<number> - set target angle\r\n"\
+  "d<number> - set (1/2) deadband\r\n"\
+  "g - start\r\n"\
+  "s - stop but stay in bang-bang mode\r\n"\
+  "v<number> - set motor speed, 0-255 default 150\r\n\n");  
 }
 
 void HelpIdle() {
   Serial << F( \
   "Control Demo code\r\n"\
-  "idle mode\r\n"\
+  "in idle mode\r\n"\
   "Use lower case commands\r\n"\
   "depending on your terminal you may need to hit enter\r\n"\
-  "q will stop the motor and return to the main loop\r\n"\
+  "q will stop the motor and return to the idle mode\r\n"\
   "h or ? - print this help\r\n"\
   "a - display angle\r\n"\
   "m - manual motor control\r\n"\
-  "b - bang-bang controller\r\n\n"\
-  "x - eXit old code, return to PID\r\n");
+  "b - bang-bang controller\r\n"\
+  "t - set up angle table\r\n"\
+  "x - eXit old code, return to PID\r\n\n");
 }
 
+void HelpManual() {
+  Serial << F(\
+  "Manual motor mode\r\n"\
+  "q will stop the motor and return to the idle mode\r\n"\
+  "h or ? - print this help\r\n"\
+  "enter an integer between -255 and 255 to set motor speed\r\n\n");
+}
 void ManualMotor() {
   int inByte, inNum;
   static int sp = 0, oldSp = 0;
 
-  inByte = Serial.peek();
-  switch( inByte ) {
-  case 'q' :
-    Quit();
-    Serial.read(); // remove character
-    return;
-    break;
-  case 'h' :
-  case '?' :
-   CDHelp();
-    Serial.read(); // remove character
-    return;
-    break;
-  case -1 : // no input
-    Serial << "speed " << sp << "  angle " << ReadAngle() << endl;
-    delay(500);
-    return;
-    break;
-  default: // assume input is a number
-    inNum = Serial.parseInt();
-    break;
-  }
-  Serial << "inByte = " << inByte << endl;
-  Serial << "inNum = " << inNum << endl;
-  oldSp = sp;
-  //limit the speed
-  sp = constrain( inNum, -255, 255 );
-  Serial   << "Changing speed from " << oldSp << " to " << sp << endl;
-  myMotor->setSpeed( abs(sp) );
-  if ( sp >= 0 ) {
+  while ( true ) {
+    inByte = Serial.peek();
+    switch( inByte ) {
+    case 'q' :
+      Quit();
+      Serial.read(); // remove character
+      return;
+      break;
+    case 'h' :
+    case '?' :
+      HelpManual();
+      Serial.read(); // remove character
+      break;
+    case -1 : // no input
+      Serial << "speed " << sp << "  angle " << ReadAngle() << endl;
+      delay(500);
+      break;
+    default: // assume input is a number
+      inNum = Serial.parseInt();
+      oldSp = sp;
+      //limit the speed
+      sp = constrain( inNum, -255, 255 );
+      Serial   << "Changing speed from " << oldSp << " to " << sp << endl;
+    }
+//    Serial << "inByte = " << inByte << endl;
+//    Serial << "inNum = " << inNum << endl;
+    myMotor->setSpeed( abs(sp) );
+    if ( sp >= 0 ) {
       myMotor->run(FORWARD);
-  }
-  else {
+    }
+    else {
       myMotor->run(BACKWARD);    
+    }
   }
 }
 
@@ -394,45 +522,44 @@ void ManualMotor() {
 void ProcessInput() {
   int byteIn;
   
-  if (Serial.available() > 0 ) {
-    byteIn = Serial.read();
+   if (Serial.available() > 0 ) {
+    byteIn = Serial.peek();
     switch( byteIn ) {
     case 'q' :
       Quit();
       break;
     case 'h' :
     case '?' :
-      CDHelp();
+      HelpIdle();
       break;
     case 'a' :
       myMotor->run(RELEASE); // make sure motor is stopped (coasting) just in case
-      oldState = state;
-      state = stDisplay;
+      state = stAngle;
+      gotNewState = true;
       break;
     case 'm' :
-      oldState = state;
       state = stManual;
+      gotNewState = true;
       break;
     case 'b' :
-      oldState = state;
       state = stBang;
+      gotNewState = true;
       break;
     case 'x' :
-      oldState = state;
       state = stExit;
-    default :  
+      gotNewState = true;
+    default : 
+      return; // avoid reading unknowm byte 
       break;
     }
+    Serial.read();
   }
 }
 
 void Quit() {
   myMotor->run(RELEASE); // make sure motor is stopped (coasting)
-  oldState = state;
   state = stIdle;
+  gotNewState = true;
 }
-
-// if at least t milliseconds has passed since last call, reset timer and return true
-// allows non-blocking timed events
 
 
